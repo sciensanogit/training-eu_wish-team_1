48b28acf3083063d1a3f481f3019e3bad861ffec
paddypea <patrick.h.peacock@gmail.com>
2025-12-12 01:08:44 +1100

    Inital check in data prep

--- 01_data_prep.R
+++ 01_data_prep.R
@@ -8,7 +8,7 @@
 
 # Load packages ----
 # select packages
-pkgs <- c("dplyr", "tidyr", "zoo", "writexl", "ggplot2")
+pkgs <- c("dplyr", "tidyr", "zoo", "writexl", "ggplot2", "stringr")
 # install packages
 install.packages(setdiff(pkgs, rownames(installed.packages())))
 invisible(lapply(pkgs, FUN = library, character.only = TRUE))
@@ -32,15 +32,18 @@ df_sc <- read.csv("https://data.geo.be/ws/sciensano/wfs?SERVICE=WFS&REQUEST=GetF
 df_pmmv <- read.csv("https://data.geo.be/ws/sciensano/wfs?SERVICE=WFS&REQUEST=GetFeature&VERSION=2.0.0&TYPENAMES=sciensano:wastewatertreatmentplantspmmv&outputFormat=csv")
 
 # join both
-df <- df_sc %>%
+df_combine <- df_sc %>%
   rbind(df_pmmv)
 
 # clean data
-df <- df %>%
-  select(siteName, collDTStart, labName, labProtocolID, flowRate, popServ, measure, value)
+df <- df_combine %>%
+  select(siteName, collDTStart, labName, labProtocolID, flowRate, popServ, quality, measure, value) %>% 
+  rename(date = collDTStart) %>% 
+  mutate(date = as.Date(date)) #%>% 
+  # mutate(across(where(is.character), as.factor))
 
 # format date
-df$date <- as.Date(df$date)
+# df$date <- as.Date(df$date)
 
 # set and subset dates
 date_reporting <- as.Date("2025-09-01", format = "%Y-%m-%d")
@@ -48,20 +51,31 @@ date_graph_start <- as.Date("2024-09-01", format = "%Y-%m-%d")
 date_graph_end <- as.Date("2025-12-01", format = "%Y-%m-%d")
 
 # subset sars and pmmv data based on labProtocolID used betwen date_start and date_end
+
+data <- df %>% 
+  # filter(str_detect(labProtocolID, "COV")) %>% 
+  filter(between(date, date_graph_start, date_graph_end)) %>% 
+
 # display existing labProtocolID
 # unique(df$labProtocolID)
-
 # rename measures
+  mutate(measure = if_else(str_detect(labProtocolID, "COV"), "SARS", "PPMV)")) %>% 
 # diplay existing measure
 # unique(df$measure)
 
 # translate siteName to english
-
-# apply LOQ provided by the lab
-
+# ("Bruxelles-Sud" to "Brussels-South", "Bruxelles-Nord" to "Brussels-North")
+  mutate(siteName = str_replace(siteName, "Bruxelles-Sud", "Brussels-South"),
+         siteName = str_replace(siteName, "Bruxelles-Nord", "Brussels-North"),
+         # apply LOQ provided by the lab         
+         value = if_else(measure == "SARS" & value < 8, NA, value),
+         value = if_else(measure == "PPMV" & value < 250, NA, value),
 # remove outliers
+# remove outlier based on the variable named "Quality". Set samples with "Quality concerns" to NA
+         value = if_else(quality == "Quality concerns", NA, value)) %>% 
 
 # compute mean of replicated analysis of each measure
+  mutate
 
 # compute viral ratio
 # unique(df$measure) ...
